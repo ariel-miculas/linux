0f3f0b9f10623277f76fe67bbc61d2df42f7c9bb
Ariel Miculas <amiculas@cisco.com>
2023-05-18 14:33:55 +0300

    rust: file: Add a new RegularFile newtype useful for reading files
    Implement from_path, from_path_in_root_mnt, read_with_offset,
    read_to_end and get_file_size methods for a RegularFile newtype.
    
    Signed-off-by: Ariel Miculas <amiculas@cisco.com>

--- rust/kernel/error.rs
+++ rust/kernel/error.rs
@@ -275,7 +275,7 @@ pub fn to_result(err: core::ffi::c_int) -> Result {
 ///     }
 /// }
 /// ```
-pub(crate) fn from_err_ptr<T>(ptr: *mut T) -> Result<*mut T> {
+pub fn from_err_ptr<T>(ptr: *mut T) -> Result<*mut T> {
     // CAST: Casting a pointer to `*const core::ffi::c_void` is always valid.
     let const_ptr: *const core::ffi::c_void = ptr.cast();
     // SAFETY: The FFI function does not deref the pointer.
