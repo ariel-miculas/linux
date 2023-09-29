b6454f7de1adf671281a43f475918ba219c5c3bf
Wedson Almeida Filho <walmeida@microsoft.com>
2023-09-29 17:58:10 -0300

    rust: fs: introduce `INode<T>`
    Allow Rust file systems to handle typed and ref-counted inodes.
    
    This is in preparation for creating new inodes (for example, to create
    the root inode of a new superblock), which comes in the next patch in
    the series.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>
    [rebased to work with latest upstream]
    Signed-off-by: Ariel Miculas Trif <amiculas@cisco.com>

--- rust/helpers/file.c
+++ rust/helpers/file.c
@@ -7,3 +7,9 @@ struct file *rust_helper_get_file(struct file *f)
     return get_file(f);
 }
 EXPORT_SYMBOL_GPL(rust_helper_get_file);
+
+loff_t rust_helper_i_size_read(const struct inode *inode)
+{
+    return i_size_read(inode);
+}
+EXPORT_SYMBOL_GPL(rust_helper_i_size_read);
